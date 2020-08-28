b0109851fd0ae156543f20acc1304f0c24b198fc
Lee Roland <glroland@hotmail.com>
2020-08-28 14:39:05 -0400

    initial version of trivia game service as generated by starter program

---
+++ trivia-game-svc/src/main/java/com/glroland/trivia/game/TriviaGameSvcApplication.java
@@ -0,0 +1,13 @@
+package com.glroland.trivia.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaGameSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviaGameSvcApplication.class, args);
+	}
+
+}
---
+++ trivia-game-svc/src/test/java/com/glroland/trivia/game/TriviaGameSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glroland.trivia.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaGameSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
